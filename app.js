6a1809e66b8a3aa80dd20916474da427a2ec56c1
root <root@localhost.localdomain>
2020-07-12 22:16:43 +0530

    DRF methods and calling functions

--- app.js
+++ app.js
@@ -6,28 +6,39 @@ var budgetController = (function(){
 
 //UI CONTROLLER
 var UIController =(function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription : '.add__description',
+        inputValue: '.add__value',
+        inputBtn:'.add__btn',
+    };
     
     return {
         getInput: function(){
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
             };
+        },
+        getDOMstrings: function(){  // create public to access
+            return DOMstrings;
         }
+
     };
 })();
 
 
 // GLOBAL APP CONTROLER
 var controller = (function(budgetCtrl, UIctrl){
+    var DOM = UIctrl.getDOMstrings(); // publically created access.
 
     var ctrlAddItem = function(){
         //1. Get the field input data.
         var input = UIctrl.getInput();
         console.log(input);
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
     document.addEventListener('keypress', function(event){
         if (event.keyCode === 13 || event.which === 13){
             ctrlAddItem();
